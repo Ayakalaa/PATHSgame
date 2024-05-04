a1c51fd675e6131bfb6ea9e024a4fabff7c82348
Ayakalaa <win7ult09@gmail.com>
2024-05-03 22:06:48 -0500

    Minor changes

--- game/logic/PATHSlogic.js
+++ game/logic/PATHSlogic.js
@@ -17,7 +17,7 @@ function nextPart(option) {
             if (option === 'Begin') {
                 currentPage = '0S';
                 document.getElementById("vs").textContent = currentPage + " - Begin"; 
-                document.getElementById("paths-text").textContent = "You’re just walking down a school hallway as normal, when all of a sudden a portal appears on the wall. <br> The portal is a swirl of colors, and you can’t decide which one it is, or where it could possibly lead to. <br> Your next class starts in a few minutes, and you can’t be late, as you’ll miss your test.";
+                document.getElementById("paths-text").innerHTML = "You’re just walking down a school hallway as normal, when all of a sudden a portal appears on the wall. <br><br> The portal is a swirl of colors, and you can’t decide which one it is, or where it could possibly lead to. <br><br> Your next class starts in a few minutes, and you can’t be late, as you’ll miss your test.";
                 document.getElementById("fontmention").textContent = "";
                 document.getElementById("mention").textContent = "";
                 document.getElementById("back").textContent = "";
@@ -27,7 +27,7 @@ function nextPart(option) {
         if (currentPage === '0S') {
             if (option === 'Option 1') {
                 currentPage = 'Path-1';
-                document.getElementById("paths-text").textContent = currentPage + " - You decide to continue down the hallway, and continue your day as usual. When you reach your class though, you appear to be the only person there. Not even the teacher is anywhere to be found. “Weird.” Sometimes the teacher is late to class, and students are commonly late. It could just be that.";
+                document.getElementById("paths-text").innerHTML = currentPage + " - You decide to continue down the hallway, and continue your day as usual. When you reach your class though, you appear to be the only person there. Not even the teacher is anywhere to be found. “Weird.” Sometimes the teacher is late to class, and students are commonly late. It could just be that.";
                 document.getElementById("vs").textContent = currentPage;
             } 
             
